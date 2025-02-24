f5f6522a83d37f10bfe00f949728377b3c4030f0
SheepFJ <125231595+SheepFJ@users.noreply.github.com>
2025-02-24 12:34:16 +0800

    更新 oilprices.js

--- QuantumultX/API/oilprices.js
+++ QuantumultX/API/oilprices.js
@@ -10,7 +10,7 @@ GitHub：https://github.com/SheepFJ/QuantumultX
 ============ Quantumult X ============
 
 [rewrite_local]
-^https:\/\/movies\.disney\.com\/sheep\/API\/oilPrices\/[^\/]+\/? url script-response-body https://raw.githubusercontent.com/SheepFJ/QuantumultX/refs/heads/main/QuantumultX/API/newstextpkc.js
+^https:\/\/movies\.disney\.com\/sheep\/API\/oilPrices\/[^\/]+\/? url script-response-body https://raw.githubusercontent.com/SheepFJ/QuantumultX/refs/heads/main/QuantumultX/API/oilprices.js
 [mitm]
 hostname = movies.disney.com
 
