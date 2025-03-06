7827bebddfc660b570006b522a357358ee94ae6e
SheepFJ <125231595+SheepFJ@users.noreply.github.com>
2025-03-06 19:29:58 +0800

    Update aireply.js

--- QuantumultX/Siri/aireply/aireply.js
+++ QuantumultX/Siri/aireply/aireply.js
@@ -1,7 +1,3 @@
 const saveResult = $prefs.valueForKey("sheep_siri_reply");
-    
-$done({
-                    status: "HTTP/1.1 200 OK",
-                    headers: { "Content-Type": "text/plain; charset=utf-8" },
-    body: saveResult
-})
+console.log(saveResult)   
+$done()
