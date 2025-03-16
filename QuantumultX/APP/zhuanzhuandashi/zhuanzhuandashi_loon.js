7f1f392fc082045636df334aea55b1ed6aa35640
SheepFJ <125231595+SheepFJ@users.noreply.github.com>
2025-03-16 19:33:30 +0800

    更新 zhuanzhuandashi_loon.js

---
+++ QuantumultX/APP/zhuanzhuandashi/zhuanzhuandashi_loon.js
@@ -0,0 +1,19 @@
+/*************************************
+项目名称：转转大师
+更新日期：2025-03-08
+脚本作者：@Sheepfj
+使用声明：⚠️仅供参考，🈲转载与售卖！
+TG频道：https://t.me/sheep_007xiaoyang
+GitHub：https://github.com/SheepFJ/QuantumultX
+脚本说明：解锁VIP
+============ Quantumult X ============
+
+[mitm]
+hostname = wxappzzds.55.la
+
+================ Loon ==================
+
+[Script]
+http-response ^https:\/\/wxappzzds\.55\.la/api/wxapp/my script-path=https://raw.githubusercontent.com/SheepFJ/QuantumultX/refs/heads/main/QuantumultX/APP/zhuanzhuandashi/zhuanzhuandashi.js,requires-body=false,tag=转转大师
+
+*************************************/
