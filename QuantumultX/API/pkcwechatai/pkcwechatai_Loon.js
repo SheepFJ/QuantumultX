7fb10687387142099ef85b6eb64c2982545fd925
SheepFJ <125231595+SheepFJ@users.noreply.github.com>
2025-05-23 15:13:53 +0800

    Add files via upload

---
+++ QuantumultX/API/pkcwechatai/pkcwechatai_Loon.js
@@ -0,0 +1,19 @@
+/*************************************
+项目名称：微信聊天机器人|PKC
+更新日期：2025-05-23
+脚本作者：@Sheepfj
+使用声明：⚠️仅供参考，🈲转载与售卖！
+TG频道：https://t.me/sheep_007xiaoyang
+GitHub：https://github.com/SheepFJ/QuantumultX
+脚本说明：用于PKC插件的微信聊天机器人接口
+============ Quantumult X ============
+
+[mitm]
+hostname = api.sheep.com 
+
+================ Loon ==================
+
+[Script]
+http-response ^https:\/\/api\.sheep\.com\/sheep\/wechatgpt\/ script-path=https://raw.githubusercontent.com/SheepFJ/QuantumultX/refs/heads/main/QuantumultX/API/pkcwechatai/pkcwechatai.js,requires-body=false,tag=微信聊天机器人
+
+*************************************/
---
+++ QuantumultX/API/pkcwechatai/pkcwechatai_QX.js
@@ -0,0 +1,18 @@
+/*************************************
+项目名称：微信聊天机器人|PKC
+更新日期：2025-05-23
+脚本作者：@Sheepfj
+使用声明：⚠️仅供参考，🈲转载与售卖！
+TG频道：https://t.me/sheep_007xiaoyang
+GitHub：https://github.com/SheepFJ/QuantumultX
+脚本说明：用于PKC插件微信聊的机器人接口
+============ Quantumult X ============
+
+[rewrite_local]
+^https:\/\/api\.sheep\.com\/sheep\/wechatgpt\/ url script-response-body https://raw.githubusercontent.com/SheepFJ/QuantumultX/refs/heads/main/QuantumultX/API/pkcwechatai/pkcwechatai.js
+[mitm]
+hostname = api.sheep.com
+
+================ Loon ==================
+
+*************************************/
