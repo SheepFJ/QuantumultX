28d41acf40dc69ade2090da48335536c5f819b44
SheepFJ <125231595+SheepFJ@users.noreply.github.com>
2025-08-20 15:32:44 +0800

    Update StudyMusicQX.js

--- StudyMusic/StudyMusicQX.js
+++ StudyMusic/StudyMusicQX.js
@@ -14,8 +14,9 @@ GitHub：https://github.com/SheepFJ/QuantumultX/
 ^https:\/\/message\.chaoxing\.com\/apis\/pmsg\/logoffUmeng url script-response-body https://raw.githubusercontent.com/SheepFJ/QuantumultX/refs/heads/main/StudyMusic/main.js
 ^https:\/\/passport2-api\.chaoxing\.com\/v11\/loginregister url script-response-header https://raw.githubusercontent.com/SheepFJ/QuantumultX/refs/heads/main/StudyMusic/main.js
 ^https:\/\/pan-yz\.chaoxing\.com\/api\/getMyDirAndFiles url script-response-body https://raw.githubusercontent.com/SheepFJ/QuantumultX/refs/heads/main/StudyMusic/main.js
-^https:\/\/((pan-yz\.chaoxing\.com)|(s2\.cldisk\.com)|(d0\.cldisk\.com)|(*\.douyinvod\.com))\/ url script-request-header https://raw.githubusercontent.com/SheepFJ/QuantumultX/refs/heads/main/StudyMusic/userinfo.js
+^https:\/\/((pan-yz\.chaoxing\.com)|(s2\.cldisk\.com)|(d0\.cldisk\.com))\/ url script-request-header https://raw.githubusercontent.com/SheepFJ/QuantumultX/refs/heads/main/StudyMusic/userinfo.js
 ^https?:\/\/.*(sycdn\.kuwo\.cn|music\.126\.net|migu\.cn|douyinvod\.com|kugou\.com)\/.* url script-response-header https://raw.githubusercontent.com/SheepFJ/QuantumultX/refs/heads/main/StudyMusic/music_dow.js
+^https?:\/\/.*\.douyinvod\.com\/.* url script-request-header https://raw.githubusercontent.com/SheepFJ/QuantumultX/refs/heads/main/StudyMusic/userinfo.js
 [mitm]
 hostname = pan-yz.chaoxing.com,message.chaoxing.com,passport2-api.chaoxing.com,s2.cldisk.com,d0.cldisk.com,*.kuwo.cn,*.126.net,*.migu.cn,*.douyinvod.com,*.kugou.com
 
