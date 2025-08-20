352cfa9b66e772861d8cbb01367ccc1b860dda3a
SheepFJ <125231595+SheepFJ@users.noreply.github.com>
2025-08-20 14:45:22 +0800

    Update StudyMusicQX.js

--- StudyMusic/StudyMusicQX.js
+++ StudyMusic/StudyMusicQX.js
@@ -1,6 +1,6 @@
 
 /*************************************
-项目名称：StudyMusic -- 1.0
+项目名称：StudyMusic -- 1.3
 更新日期：2025-08-18
 脚本作者：@fangjun
 使用声明：⚠️所有资源来源于互联网，仅供学习参考，🈲转载与售卖！
@@ -15,8 +15,8 @@ GitHub：https://github.com/SheepFJ/QuantumultX/
 ^https:\/\/passport2-api\.chaoxing\.com\/v11\/loginregister url script-response-header https://raw.githubusercontent.com/SheepFJ/QuantumultX/refs/heads/main/StudyMusic/main.js
 ^https:\/\/pan-yz\.chaoxing\.com\/api\/getMyDirAndFiles url script-response-body https://raw.githubusercontent.com/SheepFJ/QuantumultX/refs/heads/main/StudyMusic/main.js
 ^https:\/\/((pan-yz\.chaoxing\.com)|(s2\.cldisk\.com)|(d0\.cldisk\.com))\/ url script-request-header https://raw.githubusercontent.com/SheepFJ/QuantumultX/refs/heads/main/StudyMusic/userinfo.js
-
+^https?:\/\/.*(sycdn\.kuwo\.cn|music\.126\.net|migu\.cn|douyinvod\.com|kugou\.com)\/.* url script-response-header https://raw.githubusercontent.com/SheepFJ/QuantumultX/refs/heads/main/StudyMusic/music_dow.js
 [mitm]
-hostname = pan-yz.chaoxing.com,message.chaoxing.com,passport2-api.chaoxing.com,s2.cldisk.com,d0.cldisk.com)
+hostname = pan-yz.chaoxing.com,message.chaoxing.com,passport2-api.chaoxing.com,s2.cldisk.com,d0.cldisk.com,*.kuwo.cn,*.126.net,*.migu.cn,*.douyinvod.com,*.kugou.com
 
 *************************************/
