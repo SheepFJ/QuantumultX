1cd15881709e6d6f5105a738ec18f8a2e292bf57
SheepFJ <125231595+SheepFJ@users.noreply.github.com>
2025-08-25 22:23:53 +0800

    Update music_dow.js

--- StudyMusic/music_dow.js
+++ StudyMusic/music_dow.js
@@ -1,1 +1,12 @@
-$done({})
+const url = $request.url || "";
+
+if (url.includes("longzhu_api")) {
+    let headers = $response.headers || {};
+    headers["Access-Control-Allow-Origin"] = "*";
+    headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS";
+    headers["Access-Control-Allow-Headers"] = "*";
+
+    $done({ headers });
+} 
+
+$done({});
