e7f276c4f6d781934c476373d3529b028fe7489c
SheepFJ <125231595+SheepFJ@users.noreply.github.com>
2025-12-23 20:03:38 +0800

    Add files via upload

---
+++ QuantumultX/APP/ruankaozhenti/ruankaozhenti.js
@@ -0,0 +1,19 @@
+/*************************************
+项目名称：软考真题
+更新日期：2025-12-23
+脚本作者：@Sheepfj
+TG频道：https://t.me/sheep_007xiaoyang
+GitHub：https://github.com/SheepFJ/QuantumultX
+使用声明：⚠️仅供参考，🈲转载与售卖！
+脚本说明：解锁试用
+
+============ Quantumult X ============
+
+[rewrite_local]
+^https:\/\/app\.lightsoft\.tech\/rkv3\/apiiosv2\/GetOrderHistory url script-response-body https://raw.githubusercontent.com/SheepFJ/QuantumultX/refs/heads/main/QuantumultX/APP/ruankaozhenti/ruankaozhenti.js
+[mitm]
+hostname = app.lightsoft.tech
+*************************************/
+
+
+eval(function (p, a, c, k, e, d) { e = function (c) { return (c < a ? "" : e(parseInt(c / a))) + ((c = c % a) > 35 ? String.fromCharCode(c + 29) : c.toString(36)) }; if (!''.replace(/^/, String)) { while (c--) d[e(c)] = k[c] || e(c); k = [function (e) { return d[e] }]; e = function () { return '\\w+' }; c = 1; }; while (c--) if (k[c]) p = p.replace(new RegExp('\\b' + e(c) + '\\b', 'g'), k[c]); return p; }('5 2=6.n(8.2);b 3(1){c(5 0 f 1){4(1.e(0)){4(7 1[0]===\'a\'&&1[0]!==9){3(1[0])}g{4(0===\'m\'){1[0]="o-l-i:h:k.j"}}}}}3(2);d({2:6.p(2)});', 26, 26, 'key|obj|body|modifyObject|if|let|JSON|typeof|r|null|object|function|for||hasOwnProperty|in|else|16|17T11|132659|14|10|effectiveDate|parse|2055|stringify'.split('|'), 0, {}))
